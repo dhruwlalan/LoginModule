2dc3e8d52c7b7cff09614209c4140a321668bcef
dhruw <dhruwlalan22@gmail.com>
2020-10-28 19:17:20 +0530

    Add: Sign Up Page

--- src/js/form.js
+++ src/js/form.js
@@ -1,18 +1,28 @@
-
+const formTitle = document.querySelector('.form__title');
 const emailLabel = document.getElementById('emailLabel');
 const emailInput = document.getElementById('emailInput');
 const passLabel = document.getElementById('passLabel');
 const passInput = document.getElementById('passInput');
+const forSignup = document.getElementById('forsignup')
+const submitBtn = document.getElementById('submit');
+const formFooterText = document.querySelector('.form__footer--text');
+const formFooterLink = document.querySelector('.form__footer--link');
 
-// body.addEventListener('event' , callback);
-// if (document.activeElement === emailInput) {
 
-// 	console.log('true');
-// }
-
-// emailInput.addEventListener('focusin' , () => {
-// 	emailLabel.style.color = 'blue';
-// });
-// emailInput.addEventListener('focusout' , () => {
-// 	emailLabel.style.color = 'gray';
-// });
+formFooterLink.addEventListener('click' , (e) => {
+	const passConfirm = `<div class="form__input-group passConfirm"><input id="passConfInput" type="password" class="form__input"><label id="passConfLabel" for="passConfInput" class="form__label">Password Confirm</label></div>`;
+	if (e.target.textContent === 'Sign Up') {
+		submit.insertAdjacentHTML('beforebegin', passConfirm);
+		e.target.textContent = 'Login';
+		formTitle.textContent = 'Sign Up';
+		submitBtn.value = 'Sign Up';
+		formFooterText.textContent = `I'm already a member,`;
+	} else {
+		const pc = document.querySelector('.passConfirm');
+		document.querySelector('.form__body').removeChild(pc);
+		e.target.textContent = 'Sign Up';
+		formTitle.textContent = 'Login';
+		submitBtn.value = 'Login';
+		formFooterText.textContent = `I'm a new user,`;
+	}
+});
