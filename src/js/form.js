31c88cdfe3c69812d3a32702af5b3b4593a7468c
dhruw <dhruwlalan22@gmail.com>
2020-10-28 12:00:21 +0530

    Login Design complete

---
+++ src/js/form.js
@@ -0,0 +1,18 @@
+
+const emailLabel = document.getElementById('emailLabel');
+const emailInput = document.getElementById('emailInput');
+const passLabel = document.getElementById('passLabel');
+const passInput = document.getElementById('passInput');
+
+// body.addEventListener('event' , callback);
+// if (document.activeElement === emailInput) {
+
+// 	console.log('true');
+// }
+
+// emailInput.addEventListener('focusin' , () => {
+// 	emailLabel.style.color = 'blue';
+// });
+// emailInput.addEventListener('focusout' , () => {
+// 	emailLabel.style.color = 'gray';
+// });
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@
 import './assets.js';
 
 // MAIN
+import './form.js';
