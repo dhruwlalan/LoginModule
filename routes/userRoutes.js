3e36ddca5b0a06a1a400ede3d258e362db91e365
dhruw <dhruwlalan22@gmail.com>
2020-11-24 23:37:37 +0530

    RF: userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,25 +1,24 @@
-const router = require('express').Router();
+const router = require('express').Router({ strict: true });
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
+/*REST API - /api/v1/users/~*/
 
-// open routes:
+/*Open Routes*/
 router.post('/signup' , authController.signup);
 router.post('/login' , authController.login);
 router.get('/logout' , authController.logout);
 router.post('/forgetPassword' , authController.forgetPassword);
 router.patch('/resetPassword/:token' , authController.resetPassword);
 
-// logged in routes:
+/*Logged In Routes*/
 router.use(authController.protect);
-
 router.patch('/updateMyPassword' , userController.updatePassword);
 router.patch('/updateMe' , userController.uploadUserPhoto , userController.resizeUserPhoto , userController.updateMe);
 router.delete('/deleteMe' , userController.deleteMe);
 
-// admin routes:
+/*Admin Routes*/
 router.use(authController.restrictTo('admin'));
-
 router.route('/')
    .get(userController.getAllUsers)
    .post(userController.createUser);
@@ -28,4 +27,5 @@ router.route('/:id')
    .patch(userController.updateUser)
    .delete(userController.deleteUser);
 
+/*Export Router*/
 module.exports = router;
